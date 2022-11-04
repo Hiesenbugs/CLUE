1b21846046a42555d1be6f1555c350a11d7bccea
GaddipatiSriram <sriram.gc432@gmail.com>
2022-11-03 21:12:43 -0400

    pseudo code

---
+++ frontend/src/components/Lobby.vue
@@ -0,0 +1,31 @@
+class Lobby {
+    constructor(userId) {
+      this.userId = userId;
+    }
+
+    joinLobby(){
+        // 1.  ddb.put_item(this.userId)
+        // 2.  lobby_count = dd.get_count(userId)
+
+        return lobby_count
+    }
+
+    startGame(){
+        // 1. ddb.put_item(Item = {"userID" = this.userID, startGame = True})  
+        // 2. dict = ddb.scan("lobby-table-dev")
+        //    for keys,vals in dict.items():
+        //      if false in vals:
+        //        return false
+        //      else:
+        //            let game_Id = random_number(0-1000)
+        //            for player in keys:
+        //              player_object = Player(userId, gameId)
+        //              load player_object in new GamePage
+        //              load player_object + game_Id in GameTable
+        //              create winning hand Board.winningHand
+        //              redirect to new GamePage         
+    }
+
+  }
+
+
