f4d5c880c051b6fc70d051db2b42fbd6d3f52973
Rebecca Parker <rfpparker@gmail.com>
2022-11-12 15:28:56 -0600

    Fix rebase

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,17 +2,17 @@
   <div id="app">
     <h1>The Heisenbugs Present...</h1>
     <h2>CLUELESS</h2>
-    <div id="joinLobbyButton">
+    <div class="Join-Lobby">
       <input v-model="name" placeholder="Enter Name"/>
       <button :disabled="join > 0" @click="JoinLobby">Join Lobby</button>
       <figure>
         <img v-if="join > 0" src="character" alt="characterColor" width="100" height="100" />
       </figure>
     </div>
-    <div id="startButton">
-      <button :disabled="join != 1" @click="Start">Start</button>
+    <div class="Start-Game">
+      <button :disabled="LobbyCount != 4" @click="Start">Start</button>
     </div>
-    <div id="playersInLobby"> <!-- constant update on this -->
+    <div class="Players-In-Lobby"> <!-- constant update on this -->
       <span>{{lobbyCount}}</span>
     </div>
   </div>
@@ -28,34 +28,36 @@ export default {
       join: 0,
       character:'',
       characterColor:''
-    };
+    }
   },
   methods: {
-    async JoinLobby() {
+    async JoinLobby(event) {
       alert(`${this.name} has joined the game lobby.`)
-      this.join++
-      //Rand number for index of character out of array[characters]
-      //Check if character is already assigned in player table
-      //If statements for each character, assign character = ./assets/character.png and characterColor
-      let axiosConfig = {
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': "application/json",
-      }
-      };
-      var payloadBody = {  'userId': "characterColor",  } //add boolean value for startLobby = true
-      try {
-      const response = await axios.post( `https://93cpkeoc1e.execute-api.us-east-1.amazonaws.com/lobby/join/`,
-                                          payloadBody,
-                                          axiosConfig
-                                        )
-      this.lobbyCount = response.data.ResponseMetadata.HTTPStatusCode //return total players in lobby
-      } catch (e) {
-      console.log("joinLobby Error:", e)
+      if (event) {
+        this.join++
+        //Rand number for index of character out of array[characters]
+        //Check if character is already assigned in player table
+        //If statements for each character, assign character = ./assets/character.png and characterColor
+        let axiosConfig = {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': "application/json",
+        }
+        };
+        var payloadBody = {  'userId': "characterColor",  } //add boolean value for startLobby = true
+        try {
+        const response = await axios.post( `https://93cpkeoc1e.execute-api.us-east-1.amazonaws.com/lobby/join/`,
+                                            payloadBody,
+                                            axiosConfig
+                                            )
+        this.lobbyCount = response.data.ResponseMetadata.HTTPStatusCode //return total players in lobby
+        } catch (e) {
+        console.log("setLocation Error:", e)
+        }
       }
     },
-    async Start() {
-      this.$router.push('/components/GamePage')
+    Start() {
+      this.$router.push('/GamePage')
     }
   }
 }
@@ -71,36 +73,3 @@ export default {
   margin-top: 60px;
 }
 </style>
-
-<!--  
-class Lobby {
-  constructor(userId) {
-    this.userId = userId;
-  }
-
-  joinLobby(){
-      // 1.  ddb.put_item(this.userId)
-      // 2.  lobby_count = dd.get_count(userId)
-
-      return lobby_count
-  }
-
-  startGame(){
-      // 1. ddb.put_item(Item = {"userID" = this.userID, startGame = True})  
-      // 2. dict = ddb.scan("lobby-table-dev")
-      //    for keys,vals in dict.items():
-      //      if false in vals:
-      //        return false
-      //      else:
-      //            let game_Id = random_number(0-1000)
-      //            for player in keys:
-      //              player_object = Player(userId, gameId)
-      //              load player_object in new GamePage
-      //              load player_object + game_Id in GameTable
-      //              create winning hand Board.winningHand
-      //              redirect to new GamePage         
-  }
-
-}
--->
-
